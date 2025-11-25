d8280290f08d198b78297c524ae4ba1477144a13
wunpun <wunpun3@gmail.com>
2025-11-25 12:17:48 +0300

    Added burgers array and setter methods

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const burgers = ['Hamburger', 'Cheeseburger']
+
+debugger;
+
+let featuredDrink = 'Strawberry Milkshake'
+
+function addBurger () {
+    debugger;
+    const newBurger = 'Flatburger'
+    
+    burgers.push(newBurger)
+}
+
+if(true) {
+    debugger;
+    const anotherNewBurger = 'Maple Bacon Burger'
+    burgers.push(anotherNewBurger)
+}
+
+function changeFeaturedDrink () {
+    debugger;
+    featuredDrink = 'The JavaShake'
+}
